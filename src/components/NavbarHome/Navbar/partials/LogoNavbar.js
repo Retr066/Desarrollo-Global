23115c0fb2a54a343b059753e93708d10e3f340a
Jherson LP <jherson.lopez88@gmail.com>
2022-04-19 10:47:17 -0500

    modal creado

--- src/components/NavbarHome/Navbar/partials/LogoNavbar.js
+++ src/components/NavbarHome/Navbar/partials/LogoNavbar.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-import logo from "../../../assets/imgs/logo-DG.png";
-import logoBlanco from "../../../assets/imgs/logo-DG-blanco.png";
+import logo from "../../../../assets/imgs/logo-DG.png";
+import logoBlanco from "../../../../assets/imgs/logo-DG-blanco.png";
 import { useContext } from "react";
-import { ColorModeContext } from "../../../context";
+import { ColorModeContext } from "../../../../context";
 
 export const LogoNavbar = () => {
   const { isDarkMode } = useContext(ColorModeContext);
