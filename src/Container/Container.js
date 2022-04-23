3a48685e0229f8aa5d77b8b4b051b11e8632849e
Jherson LP <jherson.lopez88@gmail.com>
2022-04-23 10:11:42 -0500

    Guarando cambios

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,3 +1,3 @@
 export const Container = ({ children, className = "" }) => {
-  return <div className={`px-4 pt-20 mx-auto max-w-[95%] xl:max-w-[75%] ${className}`}>{children}</div>;
+  return <div className={`px-4 pt-20 mx-auto max-w-[95%] xl:max-w-[75%] min-h-screen ${className}`}>{children}</div>;
 };
